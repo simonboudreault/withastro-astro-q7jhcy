efa0d6700a0f18fd9f0b4f325e261187f4e16b2c
Simon Boudreault <simonboudreault04@gmail.com>
2024-10-05 08:51:19 -0400

    astro add netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind()],
-  output: 'hybrid',
+  output: 'hybrid'
   // output: 'static',
+  ,
+  adapter: netlify()
 });
