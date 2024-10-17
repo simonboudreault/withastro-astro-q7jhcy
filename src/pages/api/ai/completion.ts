719db4a696792c02f6854d283942bc86632e687c
Simon Boudreault <simonboudreault04@gmail.com>
2024-10-16 20:34:43 -0400

    debug no user

--- src/pages/api/ai/completion.ts
+++ src/pages/api/ai/completion.ts
@@ -7,15 +7,16 @@ import { useAnthropicApi } from '../../../lib/ai';
 export const POST: APIRoute = async ({ request, cookies, redirect }) => {
   const sessionResponse = await supabase.auth.getSession();
   const user = sessionResponse?.data?.session?.user;
+  console.log(sessionResponse);
   if (!user) {
     console.log('Unauthorized');
-    return new Response('Unauthorized', { status: 401 });
+    throw new Error('No user');
   }
   console.log('user', user);
   // if (user.id !== '58dbfc9a-ad5d-46cc-9ccd-9efbd3b63ff') {
   if (user.id !== '58dbfc9a-ad5d-46cc-9ccd-9efbd3b63fff') {
     console.log('Unauthorized');
-    return new Response('Unauthorized', { status: 401 });
+    throw new Error('User Unauthorized');
   }
   console.log('Authorized');
 
